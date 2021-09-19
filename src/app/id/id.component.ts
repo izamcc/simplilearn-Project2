fe760c8f45439e8a49735e6039e716693e3cd578
izamcc <izamcc5@gmail.com>
2021-09-19 23:12:53 +0300

    herkou push

---
+++ src/app/id/id.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-id',
+  templateUrl: './id.component.html',
+  styleUrls: ['./id.component.scss']
+})
+export class IdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  Submit(idForm) {
+    console.log('Submitted', idForm);
+  }
+}
---
+++ src/app/id/id.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IdComponent } from './id.component';
+
+
+
+@NgModule({
+  declarations: [
+    IdComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IDModule { }
